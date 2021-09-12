f3a36c56291dbe5da49a0b8bcc25168f1a237844
PinkGoosik <MCGycuk@yandex.ru>
2021-09-12 15:28:04 +0500

    0.1.3

--- src/main/java/ru/pinkgoosik/visuality/VisualityMod.java
+++ src/main/java/ru/pinkgoosik/visuality/VisualityMod.java
@@ -1,28 +1,24 @@
 package ru.pinkgoosik.visuality;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.EntityType;
 import ru.pinkgoosik.visuality.api.HitMobParticleRegistry;
 import ru.pinkgoosik.visuality.event.VisualityEvents;
 import ru.pinkgoosik.visuality.particle.VisualityParticles;
 
-public class VisualityMod implements ModInitializer, ClientModInitializer {
+public class VisualityMod implements ClientModInitializer {
 
 	public static final String MOD_ID = "visuality";
-
-	@Override
-	public void onInitialize() {
-        HitMobParticleRegistry.register(EntityType.SKELETON, VisualityParticles.BONE, 15);
-        HitMobParticleRegistry.register(EntityType.SKELETON_HORSE, VisualityParticles.BONE, 15);
-        HitMobParticleRegistry.register(EntityType.STRAY, VisualityParticles.BONE, 15);
-        HitMobParticleRegistry.register(EntityType.WITHER_SKELETON, VisualityParticles.WITHER_BONE, 20);
-        HitMobParticleRegistry.register(EntityType.CHICKEN, VisualityParticles.FEATHER, 3);
-	}
 
 	@Override
 	public void onInitializeClient() {
 		VisualityParticles.register();
 		VisualityEvents.registerClient();
+
+		HitMobParticleRegistry.register(EntityType.SKELETON, VisualityParticles.BONE, 15);
+		HitMobParticleRegistry.register(EntityType.SKELETON_HORSE, VisualityParticles.BONE, 15);
+		HitMobParticleRegistry.register(EntityType.STRAY, VisualityParticles.BONE, 15);
+		HitMobParticleRegistry.register(EntityType.WITHER_SKELETON, VisualityParticles.WITHER_BONE, 20);
+		HitMobParticleRegistry.register(EntityType.CHICKEN, VisualityParticles.FEATHER, 3);
 	}
 }
--- src/main/java/ru/pinkgoosik/visuality/mixin/LivingEntityMixin.java
+++ src/main/java/ru/pinkgoosik/visuality/mixin/LivingEntityMixin.java
@@ -4,32 +4,53 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.item.Item;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.SwordItem;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.pinkgoosik.visuality.api.HitMobParticleRegistry;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
+    @Shadow public abstract boolean isAlive();
+
+    int ticksDelay = 0;
+
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void tick(CallbackInfo ci){
+        if(world.isClient && ticksDelay != 0) ticksDelay--;
+    }
+
     @Inject(method = "damage", at = @At("HEAD"))
     void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir){
-
-        if(source.getAttacker() != null && amount >= 2 && this.getEntityWorld() instanceof ServerWorld world){
-            HitMobParticleRegistry.getEntries().forEach((entry) -> {
+        if(world.isClient && source.getAttacker() instanceof LivingEntity attacker && ticksDelay == 0 && this.isAlive()){
+            HitMobParticleRegistry.getEntries().forEach(entry -> {
                 if(this.getType().equals(entry.entityType())){
-                    for(int i = 1; i <= (int)amount / 2; i++){
-                        world.spawnParticles(entry.particleEffect(), this.getX(), this.getY() + 0.5 + (double)this.random.nextInt(entry.height()) / 10, this.getZ(), 0, 0, 0, 0, 0);
-                    }
+                    ticksDelay = 10;
+                    Item itemInHand = attacker.getStackInHand(Hand.MAIN_HAND).getItem();
+                    if(itemInHand instanceof SwordItem swordItem) spawnHitParticles(entry, (int)swordItem.getAttackDamage() / 2);
+                    else if(itemInHand instanceof MiningToolItem miningToolItem) spawnHitParticles(entry, (int)miningToolItem.getAttackDamage() / 2);
+                    else spawnHitParticles(entry, this.random.nextInt(2));
                 }
             });
+        }
+    }
+
+    private void spawnHitParticles(HitMobParticleRegistry.Entry entry, int count){
+        for(int i = 0; i <= count; i++){
+            world.addParticle(entry.particleEffect(), this.getX(), this.getY() + 0.5 + (double)this.random.nextInt(entry.height()) / 10, this.getZ(), 0, 0, 0);
         }
     }
 }
